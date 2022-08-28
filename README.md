2935f06e4d845c950ff48c4964ba07509cd983fd
grrakesh <rakesh.gr@piddigital.com>
2022-08-28 15:03:12 +0530

    AWS CDK workshop

--- README.md
+++ README.md
@@ -1,15 +1,20 @@
 # Welcome to your CDK TypeScript project
 
 You should explore the contents of this project. It demonstrates a CDK app with an instance of a stack (`Workshop1Stack`)
-which contains an Amazon SQS queue that is subscribed to an Amazon SNS topic.
+which contains an Amazon Lambda, Dynamodb, API gateway with providing IAM roles and access permission.
 
 The `cdk.json` file tells the CDK Toolkit how to execute your app.
+
 
 ## Useful commands
 
 * `npm run build`   compile typescript to js
 * `npm run watch`   watch for changes and compile
 * `npm run test`    perform the jest unit tests
-* `cdk deploy`      deploy this stack to your default AWS account/region
+
+* `cdk bootstrap`   Calls for the first time during cdk deployment
 * `cdk diff`        compare deployed stack with current state
 * `cdk synth`       emits the synthesized CloudFormation template
+* `cdk deploy`      deploy this stack to your default AWS account/region
+* `cdk deploy --hotswap` Use it when you want to push only code changes and not the stack
+* `cdk watch`       watch the changes of the file and automatically it calls the cdk deploy internally, it pushes only code changes
--- lib/workshop1-stack.ts
+++ lib/workshop1-stack.ts
@@ -1,19 +1,29 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway'
+import { HitCounter } from './hitcounter';
 import { Construct } from 'constructs';
 
-export class Workshop1Stack extends Stack {
+export class Workshop1Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'Workshop1Queue', {
-      visibilityTimeout: Duration.seconds(300)
+    //Defines an AWS lambda resource
+    const hello = new lambda.Function(this, "HelloHandler", {
+      runtime: lambda.Runtime.NODEJS_14_X,    //execution environment
+      code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
+      handler: 'hello.handler'                // file is "hello", function is "handler"
     });
 
-    const topic = new sns.Topic(this, 'Workshop1Topic');
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter',{
+      downstream: hello
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    //defines an API gateway REST API resource backed by out "hello" function.
+    new apigw.LambdaRestApi(this, 'Endpoint',{
+      handler: helloWithCounter.handler
+    });
+    
   }
 }
